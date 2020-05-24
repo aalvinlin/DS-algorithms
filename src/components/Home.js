7f1c637c50d2aa9efc1cef364cedd241ab287007
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-24 16:56:07 -0700

    remove reaviz dependency

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ScatterPlot } from "reaviz";
 import { Scatter } from "react-chartjs-2";
 
 import { generateClusterEllipse, generateClusterCircle } from "../utils/generateClusters";
@@ -9,7 +8,6 @@ const Home = () => {
     let cluster1 = generateClusterEllipse(100, 10, 10, 20, 7, 0, 1);
     let cluster2 = generateClusterEllipse(100, 80, 70, 12, 20, 0, 0);
     let cluster3 = generateClusterCircle(100, 30, 90, 10, 0);
-    let clusterReformatted = cluster1.map((point, id) => { return {"key": point[0], "data": point[1], id}});
 
     let chartJSData = {
         datasets: [
@@ -31,7 +29,6 @@ const Home = () => {
     return (
         <>
             <h1>Home</h1>
-            <ScatterPlot height={300} width={300} data={clusterReformatted} />
             <Scatter data={chartJSData} />
         </>
     )
