e8a163a1bac909d1d6836fb0587b34521983a33f
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-25 14:38:22 -0700

    add function to sort coordinate points by x- and then by y-values

--- src/components/Dbscan.js
+++ src/components/Dbscan.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { Scatter } from "react-chartjs-2";
+
+import { set1, set2, set3 } from "../data/data";
+import { createClusters } from "../utils/generateClusters";
+import { sortCoordinates } from "../utils/sortCoordinates";
 
 const Dbscan = () => {
+
+    let scatterPlot1 = createClusters(set2);
+
+    // sort by increasing x-coordinates
+    // if x-coordinates are the same, sort by increasing y-coordinates
+    sortCoordinates(scatterPlot1);
+
+    console.log(scatterPlot1)
+
+    let chartJSData = {
+        datasets: [
+            {
+                label: "Scatter Plot #1",
+                pointBackgroundColor: "#333333",
+                data: scatterPlot1
+
+            }
+        ]
+    };
+
     return (
-        <h1>DBSCAN</h1>
+        <>
+            <h1>DBSCAN</h1>
+            <Scatter data={chartJSData} />
+        </>
     )
 }
 
