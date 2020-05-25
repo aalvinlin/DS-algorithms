a61ed711e67e234b9af35547f52b07e7056d4f01
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-24 18:39:24 -0700

    complete function to generate an arc of points

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
     let cluster1 = generateClusterEllipse(100, 10, 10, 20, 7, 0, 0.1);
     let cluster2 = generateClusterEllipse(100, 80, 70, 12, 20, 0, 0.1);
     let cluster3 = generateClusterCircle(100, 30, 90, 10, 0.1);
-    let cluster4 = generateClusterArc(100, 20, 50, 20, 15, 0, "less", 0.1);
+    let cluster4 = generateClusterArc(100, 20, 50, 20, 15, 30, "less", 0.1);
+    let cluster5 = generateClusterArc(100, 35, 50, 20, 15, 30, "greater", 0.1);
 
     let chartJSData = {
         datasets: [
@@ -27,6 +28,10 @@ const Home = () => {
             {
                 pointBackgroundColor: "#9933FF",
                 data: cluster4.map((point, id) => { return {"x": point[0], "y": point[1]}})
+            },
+            {
+                pointBackgroundColor: "#CC3300",
+                data: cluster5.map((point, id) => { return {"x": point[0], "y": point[1]}})
             }
 
         ]
--- src/utils/generateClusters.js
+++ src/utils/generateClusters.js
@@ -37,13 +37,30 @@ export const generateClusterCircle = (numberOfPoints, h, k, r, randomOffset=0) =
     return generateClusterEllipse(numberOfPoints, h, k, r, r, 0, randomOffset);
 }
 
-export const generateClusterArc = (numberOfPoints, h, k, rOuter, rInner, angle, direction, randomOffset=0) => {
-    
+export const generateClusterArc = (numberOfPoints, h, k, rOuter, rInner, angle=0, direction="greater", randomOffset=0) => {
+
+    // return nothing if the inner radius is too big
     if (rInner >= rOuter)
         { return []; }
 
     let pointsSoFar = 0;
     let points = [];
+    let slope = null || angle;
+
+    // determine equation of line to check
+    if (slope) {
+
+        if (angle % 180 === 90)
+            { slope = "vertical" }
+        
+        else
+            {
+                // convert angle in degrees to a ratio
+                slope = Math.tan(angle / 180 * Math.PI);
+            }
+
+        console.log("slope is", slope)
+    }
 
     // 1/n chance of adding a point outside the shape
     let chanceOfAddingOutsidePoint = 50;
@@ -59,9 +76,24 @@ export const generateClusterArc = (numberOfPoints, h, k, rOuter, rInner, angle, 
                     ((randomPointX - h) ** 2 / (rOuter ** 2) + (randomPointY - k) ** 2 / (rOuter ** 2) <= 1
                         && (randomPointX - h) ** 2 / (rInner ** 2) + (randomPointY - k) ** 2 / (rInner ** 2) >= 1)
 
-                    // || (!Math.floor(Math.random() * chanceOfAddingOutsidePoint))
+
+                    // if slope is specified, ensure that the point is either above/below the diagonal line specified or left/right of the vertical line
+                    && (!slope
+                        || (slope &&
+                                (
+                                    (slope !== "vertical" && direction === "greater" && randomPointY - k >= slope * (randomPointX - h))
+                                    // (console.log(slope, direction) && slope !== "vertical" && direction === "less" && console.log("yay"))
+                                    || (slope !== "vertical" && direction === "less" && randomPointY - k <= slope * (randomPointX - h))
+                                    // vertical slope: keep x-values that are either greater than or less than h
+                                    || (slope === "vertical" && direction === "less" && randomPointX <= h)
+                                    || (slope === "vertical" && direction === "greater" && randomPointX >= h)
+                                )
+                            )
+                        )
                     )
                 {
+                    console.log(randomPointY - k <= slope * (randomPointX - h))
+
                     // move point randomly by specified offset
                     randomPointX += Math.floor(Math.floor(1 / randomOffset / 2) * Math.random()) * randomOffset * oneOrNegativeOne();
                     randomPointY += Math.floor(Math.floor(1 / randomOffset / 2) * Math.random()) * randomOffset * oneOrNegativeOne();
